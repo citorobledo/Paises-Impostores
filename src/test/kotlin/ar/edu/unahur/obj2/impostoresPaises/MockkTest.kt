accdabb78e25219f5a776ec15815c17ad80058e6
Javier Robledo <82849725+citorobledo@users.noreply.github.com>
2022-01-01 13:50:37 -0300

    se agrego el pais francia. se eliminaronlos paises repetidos. se agrego la conversion del euro a dolar. los paises por codigo ahora llaman al pais mockkeado. ahora el metodo que llama a todos los paises, no tiene una lista de countries, sino que tiene una lista de llamados a buscar paises.

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/MockkTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/MockkTest.kt
@@ -9,7 +9,7 @@ import io.mockk.mockk
 
 class MockTests : DescribeSpec({
   //Etapa 4
-  describe("Pruebas con mock") {
+  describe("Pruebas con mockk") {
     val apiPaisesMock = mockk<RestCountriesAPI>()
     val apiCurrenciMock = mockk<CurrencyConverterAPI>()
     val observatorio = Observatorio(apiPaisesMock, apiCurrenciMock)
@@ -19,65 +19,8 @@ class MockTests : DescribeSpec({
     every { apiCurrenciMock.convertirDolarA("CLP")} returns 0.0012
     every { apiCurrenciMock.convertirDolarA("JMD")} returns 0.0064
     every { apiCurrenciMock.convertirDolarA("PYG")} returns 0.00015
-
-    every { apiPaisesMock.todosLosPaises() } returns
-            listOf(
-              //Argentina
-              Country("Argentina", "ARG", "Buenos Aires", "Americas", 45376763,
-                2780400.0, listOf("BRA", "CHL", "PRY"), listOf(Language("Spanish"), Language("Guaraní")),
-                listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("ARS"))),
-              //Chile
-              Country("Chile", "CHL", "Santiago", "Americas", 19116209, 756102.0, listOf("ARG"),
-                listOf(Language("Spanish")), listOf(
-                  RegionalBloc(acronym="PA", name="Pacific Alliance"),
-                  RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("CLP"))),
-              //Paraguay
-              Country("Paraguay", "PRY", "Asunción", "Americas", 7132530, 406752.0, listOf("ARG", "BRA"),
-                listOf(Language("Spanish"), Language("Guaraní")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("PYG"))),
-              //Jamaica
-              Country("Jamaica", "JAM", "Kingston", "Americas", 2961161, 10991.0, listOf(), listOf(Language("English")),
-                listOf(RegionalBloc(acronym="CARICOM", name="Caribbean Community")), listOf(Currency("JMD"))),
-              //Brazil
-              Country("Brazil", "BRA", "Brasília", "Americas", 212559409, 8515767.0, listOf("ARG", "PRY"),
-                listOf(Language("Portuguese")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")), listOf(
-                  Currency("BRL")))
-            )
-
-    every { apiPaisesMock.buscarPaisesPorNombre("Argentina")} returns
-            listOf(
-              Country("Argentina", "ARG", "Buenos Aires", "Americas", 45376763,
-                2780400.0, listOf("BRA", "CHL", "PRY"), listOf(Language("Spanish"), Language("Guaraní")),
-                listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("ARS"))))
-
-    every { apiPaisesMock.buscarPaisesPorNombre("Chile")} returns
-            listOf(
-              Country("Chile", "CHL", "Santiago", "Americas", 19116209, 756102.0, listOf("ARG"),
-                listOf(Language("Spanish")), listOf(
-                  RegionalBloc(acronym="PA", name="Pacific Alliance"),
-                  RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("CLP"))))
-
-    every { apiPaisesMock.buscarPaisesPorNombre("Paraguay")} returns
-            listOf(
-              Country("Paraguay", "PRY", "Asunción", "Americas", 7132530, 406752.0, listOf("ARG", "BRA"),
-                listOf(Language("Spanish"), Language("Guaraní")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-                listOf(Currency("PYG"))))
-
-    every { apiPaisesMock.buscarPaisesPorNombre("Jamaica")} returns
-            listOf(
-              Country("Jamaica", "JAM", "Kingston", "Americas", 2961161, 10991.0, listOf(), listOf(Language("English")),
-                listOf(RegionalBloc(acronym="CARICOM", name="Caribbean Community")), listOf(Currency("JMD"))))
-
-    every { apiPaisesMock.buscarPaisesPorNombre("Brazil")} returns
-            listOf(
-              Country("Brazil", "BRA", "Brasília", "Americas", 212559409, 8515767.0, listOf("ARG", "PRY"),
-                listOf(Language("Portuguese")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")), listOf(
-                  Currency("BRL"))))
-
+    every { apiCurrenciMock.convertirDolarA("EUR")} returns 1.1
+// Paises
     every { apiPaisesMock.buscarPaisesPorNombre("Argentina")} returns
             listOf(
               Country("Argentina", "ARG", "Buenos Aires", "Americas", 45376763,
@@ -110,32 +53,39 @@ class MockTests : DescribeSpec({
                 listOf(Language("Portuguese")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")), listOf(
                   Currency("BRL"))))
 
+    every { apiPaisesMock.buscarPaisesPorNombre("Francia")} returns
+    listOf(
+      Country("Francia", "FRA", "Paris", "Europa", 40567900, 8515.0, listOf("ARG", "BRA"),
+        listOf(Language("Frances")), listOf(RegionalBloc(acronym="Euro", name="Union Europea")), listOf(
+                            Currency("EUR"))))
+// Paises por codigo
     every { apiPaisesMock.paisConCodigo("ARG")} returns
-            Country("Argentina", "ARG", "Buenos Aires", "Americas", 45376763,
-              2780400.0, listOf("BRA", "CHL", "PRY"), listOf(Language("Spanish"), Language("Guaraní")),
-              listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-              listOf(Currency("ARS")))
+            apiPaisesMock.buscarPaisesPorNombre("Argentina").first()
 
     every { apiPaisesMock.paisConCodigo("CHL")} returns
-            Country("Chile", "CHL", "Santiago", "Americas", 19116209, 756102.0, listOf("ARG"),
-              listOf(Language("Spanish")), listOf(
-                RegionalBloc(acronym="PA", name="Pacific Alliance"),
-                RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-              listOf(Currency("CLP")))
+            apiPaisesMock.buscarPaisesPorNombre("Chile").first()
 
     every { apiPaisesMock.paisConCodigo("PRY")} returns
-            Country("Paraguay", "PRY", "Asunción", "Americas", 7132530, 406752.0, listOf("ARG", "BRA"),
-              listOf(Language("Spanish"), Language("Guaraní")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")),
-              listOf(Currency("PYG")))
+            apiPaisesMock.buscarPaisesPorNombre("Paraguay").first()
 
     every { apiPaisesMock.paisConCodigo("JAM")} returns
-            Country("Jamaica", "JAM", "Kingston", "Americas", 2961161, 10991.0, listOf(), listOf(Language("English")),
-              listOf(RegionalBloc(acronym="CARICOM", name="Caribbean Community")), listOf(Currency("JMD")))
+            apiPaisesMock.buscarPaisesPorNombre("Jamaica").first()
 
     every { apiPaisesMock.paisConCodigo("BRA")} returns
-            Country("Brazil", "BRA", "Brasília", "Americas", 212559409, 8515767.0, listOf("ARG", "PRY"),
-              listOf(Language("Portuguese")), listOf(RegionalBloc(acronym="USAN", name="Union of South American Nations")), listOf(
-                Currency("BRL")))
+            apiPaisesMock.buscarPaisesPorNombre("Brazil").first()
+
+    every { apiPaisesMock.paisConCodigo("FRA")} returns
+            apiPaisesMock.buscarPaisesPorNombre("Francia").first()
+// Todos los paises
+    every { apiPaisesMock.todosLosPaises() } returns
+            listOf(
+              apiPaisesMock.buscarPaisesPorNombre("Argentina").first(),
+              apiPaisesMock.buscarPaisesPorNombre("Chile").first(),
+              apiPaisesMock.buscarPaisesPorNombre("Paraguay").first(),
+              apiPaisesMock.buscarPaisesPorNombre("Jamaica").first(),
+              apiPaisesMock.buscarPaisesPorNombre("Brazil").first(),
+              apiPaisesMock.buscarPaisesPorNombre("Francia").first()
+            )
 
     describe("Saber si") {
       it("Chile y Argentina son limitrofes") {
@@ -166,7 +116,7 @@ class MockTests : DescribeSpec({
         observatorio.aCuantoEquivaleEn("Chile", 150.00, "Argentina").shouldBe(1238.0)
       }
       it("Los 5 paises con mayor densidad habitacional son Brasil, Argentina, Chile, Paraguay y Jamaica") {
-        observatorio.codigosISODe5PaisesMasPoblados().shouldBe(listOf("BRA", "ARG", "CHL", "PRY", "JAM"))
+        observatorio.codigosISODe5PaisesMasPoblados().shouldBe(listOf("BRA", "ARG", "FRA", "CHL", "PRY"))
       }
       it("El continente con paises mas plurinacionales es el continente Americano") {
         observatorio.continenteConMasPaisesPlurinacionales().shouldBe("Americas")
