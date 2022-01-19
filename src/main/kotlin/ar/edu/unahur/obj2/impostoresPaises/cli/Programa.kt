d0176d80d14c64268f1dfb06830b905c7219d487
Javier Robledo <82849725+citorobledo@users.noreply.github.com>
2022-01-18 23:28:39 -0300

    se elimino codigo comentado en Programa.kt. al constructor Programa.kt se le agrego la variable numeroOpcion. se refactorizo elmetodo iniciar. se separo en sub tareas y se corrigieron errores menores de esteticos.

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Programa.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Programa.kt
@@ -2,6 +2,7 @@ package ar.edu.unahur.obj2.impostoresPaises.cli
 import ar.edu.unahur.obj2.impostoresPaises.api.RestCountriesAPI
 import ar.edu.unahur.obj2.impostoresPaises.Observatorio
 import ar.edu.unahur.obj2.impostoresPaises.api.Apis
+import kotlin.system.exitProcess
 
 // El código de nuestro programa, que (eventualmente) interactuará con una persona real
 //object Programa {
@@ -33,15 +34,19 @@ import ar.edu.unahur.obj2.impostoresPaises.api.Apis
 class Programa (
   var entradaSalida: Consola = Consola,
   var apiPaises: RestCountriesAPI = Apis.paises(),
-  var observatorio: Observatorio = Observatorio(apiDePaises = apiPaises)
+  var observatorio: Observatorio = Observatorio(apiDePaises = apiPaises),
+  var numeroOpcion : String = "0"
 ){
   // Ambas son variables para poder cambiarlas en los tests
 
   fun iniciar() {
-    var numeroOpcion: Int?
-    fun opciones(){
-      entradaSalida.escribirLinea("""
-      Hola, escribí el número de la opción que deseas saber:
+    while (numeroOpcion.toInt() != 9) {
+      procesoPedidoRespuesta()
+    }
+  }
+  fun escribeOpciones(){
+    entradaSalida.escribirLinea("""
+      Hola, escribí el número de la opción que deseas saber :
       1) Si son limítrofes.
       2) Si necesitan traductor para dialogar.
       3) Si son potenciales aliados.
@@ -50,41 +55,32 @@ class Programa (
       6) El codigos de los 5 paises más poblados.
       7) Cual es el continente con más paises plurinacionales
       8) Cual es el promedio de poblacion en islas
+      9) Salir
       """.trimIndent())
+  }
+  fun numeroIncorrecto(numero: String) {
+      if (numero.equals(Char) || numero.toInt() > 9) entradaSalida.escribirLinea("El número no es válido.\n")
     }
+  fun procesoPedidoRespuesta(){
     do {
-      //entradaSalida.escribirLinea("""
-      //Hola, escribí el número de la opción que deseas saber:
-      //1) Si son limítrofes.
-      //2) Si necesitan traductor para dialogar.
-      //3) Si son potenciales aliados.
-      //4) Si conviene ir de compras de un pasi a otro.
-      //5) Al Convertir moneda de un pais al otro.
-      //6) El codigos de los 5 paises más poblados.
-      //7) Cual es el continente con más paises plurinacionales
-      //8) Cual es el promedio de poblacion en islas
-      //""".trimIndent())
-      opciones()
-      numeroOpcion = entradaSalida.leerLinea()?.toIntOrNull()
-
-
+      escribeOpciones()
+      numeroOpcion = entradaSalida.leerLinea().toString()
+      numeroIncorrecto(numeroOpcion)
     }
-    while ( numeroOpcion == null || numeroOpcion > 8 )
+    while (numeroOpcion == "" || numeroOpcion.toInt() > 9)
 
-    when (numeroOpcion) {
-      1 -> { chequearCondicion("son limítrofes.", observatorio::sonLimitrofes) }
-      2 -> { chequearCondicion("nesecitan traduccion.", observatorio::necesitanTraduccion) }
-      3 -> { chequearCondicion("son potenciales aliados.", observatorio::sonPotencialesAliados) }
-      4 -> { chequearCondicion("conviene ir de compras.", observatorio::convieneIrDeCompras) }
+    when (numeroOpcion.toInt()) {
+      1 -> { chequearCondicion("son limítrofes.\n", observatorio::sonLimitrofes) }
+      2 -> { chequearCondicion("nesecitan traduccion.\n", observatorio::necesitanTraduccion) }
+      3 -> { chequearCondicion("son potenciales aliados.\n", observatorio::sonPotencialesAliados) }
+      4 -> { chequearCondicion("conviene ir de compras.\n", observatorio::convieneIrDeCompras) }
       5 -> { convertirMoneda() }
       6 -> { paisesDeMayorDensidad() }
       7 -> { continenteMasPaisesPlurinacionales() }
       8 -> { promedioPoblacionIslas() }
+      9 -> { exitProcess(0) }
     }
-  }
 
-  fun numeroIncorrecto(numeroOpcion: Int) {
-    if (numeroOpcion > 8) entradaSalida.escribirLinea("El número no es válido.")
   }
   fun convertirMoneda() {
     val (pais, pais2) = pedirPaises()
@@ -95,20 +91,19 @@ class Programa (
   }
   fun paisesDeMayorDensidad() {
     entradaSalida.escribirLinea(
-      "Los códigos ISO de los cinco paises más poblados son: ${observatorio.codigosISODe5PaisesMasPoblados()/*los5PaisesConMayorDensidad()*/}"
+      "Los códigos ISO de los cinco paises más poblados son: ${observatorio.codigosISODe5PaisesMasPoblados()/*los5PaisesConMayorDensidad()*/} \n"
     )
   }
   fun continenteMasPaisesPlurinacionales() {
     entradaSalida.escribirLinea(
-      "El continente con más paises plurinacionales es: ${observatorio.continenteConMasPaisesPlurinacionales()}"
+      "El continente con más paises plurinacionales es: ${observatorio.continenteConMasPaisesPlurinacionales()} \n"
     )
   }
   fun promedioPoblacionIslas(){
     entradaSalida.escribirLinea(
-      "El promedio de poblacion en islas es de: ${observatorio.promedioDePoblacionEnIslas()} personas"
+      "El promedio de poblacion en islas es de: ${observatorio.promedioDePoblacionEnIslas()} personas \n"
     )
   }
-
   fun pedirPaises(): Array<String> {
     var pais: String
     do {
@@ -141,18 +136,17 @@ class Programa (
     }
     return arrayOf(pais,pais2)
   }
-
   fun ambosExisten(pais1: String, pais2: String) = observatorio.existe(pais1) && observatorio.existe(pais2)
   fun chequearCondicion(mensaje: String, condicion: (pais1: String, pais2: String) -> Boolean) {
     val (pais, pais2) = pedirPaises()
 
-    if(ambosExisten(pais, pais2)){
-      if(condicion(pais, pais2)){
+    if (ambosExisten(pais, pais2)) {
+      if (condicion(pais, pais2)) {
         entradaSalida.escribirLinea(mensaje)
-      }
-      else {
+      } else {
         entradaSalida.escribirLinea("No " + mensaje)
       }
     }
   }
 }
+
